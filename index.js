695f5b9b1498e62229255f9213d87bfe7a6281b7
Pim de Wit <pim@pdw.io>
2019-05-24 19:53:38 +0100

    Added babel to plugins obj rather than configs (oops)

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 module.exports = {
   "extends": [
     "plugin:react/recommended",
-    "google",
-    "babel"
+    "google"
   ],
   "parserOptions": {
     "sourceType": "module",
@@ -10,6 +9,7 @@ module.exports = {
   },
   "plugins": [
     "react",
+    "babel",
     "require-jsdoc-except"
   ],
   "env": {
